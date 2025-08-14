c3e28f57061cb584b8b2776dc002e1843ef4ec54
VeveCambor <121693001+VeveCambor@users.noreply.github.com>
2025-08-15 00:09:07 +0200

    Update CSS styles

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("62d149411f8f0294")
+/******/ 		__webpack_require__.h = () => ("cf6e3a5a96b6a5c3")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c9f17f9510718220"; }
+/******/ 		__webpack_require__.h = function() { return "bcad2f95fad51688"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
